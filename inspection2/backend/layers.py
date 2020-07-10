96d7e7b4ff810ac28539f7ed482214c25ee0cd2d
Shuai <47725674+shuaih7@users.noreply.github.com>
2020-07-10 17:57:06 +0800

    minor change

---
+++ inspection2/backend/layers.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Header ...
+
+import tensorflow as tf
+from tensorflow.keras.layers as layers
+#import Input, InputLayer
+#from tensorflow.keras.layers import Conv2d, Dense, BatchNormalization, MaxPooling2D, Activation, Flatten
+
+
+def Input(input_shape):
+    pass
+
